80e400a8268cf5b2e966bccb899f2e39b5b38542
Henry Peterson <hankpete97@gmail.com>
2018-07-12 10:59:21 -0700

    plotting on lenovo

--- plot_composites.py
+++ plot_composites.py
@@ -80,8 +80,95 @@ def plot2dxy_lag(filenames, lags=[-2,-1,0,1,2], lvl=850):
             ax.set_title('lag ${}$'.format(lag))
     plt.tight_layout()
 
-def plot2dxy_lag_vector(ufilenames, vfilenames, ufilenames_ttest=None, vfilenames_ttest=None, title='850 mb', lags=[-2,-1,0,1,2], lvl=850):
+def plot2dxy_lag_vector_indiv(ufilenames, vfilenames,
+                        ufilenames_ttest=None, 
+                        vfilenames_ttest=None, 
+                        lags=[-2,-1,0,1,2], 
+                        lvl=850, mode='biweekly', 
+                        phases='1234', 
+                        amp='strong'):
     # setup
+    title='Significant Anomalous Wind\n({} mb, n = {}, {} {} mode phases {})'.format(lvl, n, amp, mode, phases) 
+    proj = ccrs.PlateCarree()
+    #llon = 150; rlon = 210; blat = -25; tlat = 25
+    llon = 130; rlon = 230; blat = -55; tlat = 30
+
+    # loop
+    for i in range(len(lags)):
+        f = plt.figure(figsize=(8,8))
+        ax = plt.subplot(111, projection=proj)
+        lag = lags[i]
+
+        # map
+        ax.add_feature(cfeature.OCEAN, zorder=0, color='#7777cc')
+        ax.add_feature(cfeature.LAND,  zorder=0, color='#77cc77')
+        ax.coastlines(resolution='110m', color='black', linewidth=0.5)
+        gl = ax.gridlines(crs=proj, draw_labels=True,
+            linewidth=0.5, color='w', alpha=0.6, linestyle='--')
+        gl.xlabels_top = False
+        gl.ylabels_right = False
+        ax.set_xmargin(0)
+        ax.set_ymargin(0)
+
+        # data
+        ufilenames_full = idir + ufilenames
+        vfilenames_full = idir + vfilenames
+        if ufilenames_ttest:
+            ufilenames_ttest_full = idir + ufilenames_ttest
+            vfilenames_ttest_full = idir + vfilenames_ttest
+            u_da_raw = xr.open_dataarray(ufilenames_full+str(lag)+'.nc')
+            v_da_raw = xr.open_dataarray(vfilenames_full+str(lag)+'.nc')
+            u_da_sig = xr.open_dataarray(ufilenames_ttest_full+str(lag)+'.nc')
+            v_da_sig = xr.open_dataarray(vfilenames_ttest_full+str(lag)+'.nc')
+            Uraw = u_da_raw.values
+            Vraw = v_da_raw.values
+            Usig = u_da_sig.values
+            Vsig = v_da_sig.values
+            # if either component is significant, draw
+            Usig[np.where(Vsig != 0.0)] = Uraw[np.where(Vsig != 0.0)]
+            Vsig[np.where(Usig != 0.0)] = Vraw[np.where(Usig != 0.0)]
+            # else kill
+            Usig[np.where(Usig == 0.0)] = np.nan
+            Vsig[np.where(Vsig == 0.0)] = np.nan
+            u_da = u_da_raw
+            v_da = v_da_raw
+            u_da.values = Usig
+            v_da.values = Vsig
+        else:
+            u_da = xr.open_dataarray(ufilenames_full+str(lag)+'.nc')
+            v_da = xr.open_dataarray(vfilenames_full+str(lag)+'.nc')
+
+        u_comp = u_da.sel({'lvl' : lvl}, method='nearest').sel(
+            {'lat' : slice(tlat, blat), 'lon' : slice(llon, rlon)})
+        v_comp = v_da.sel({'lvl' : lvl}, method='nearest').sel(
+            {'lat' : slice(tlat, blat), 'lon' : slice(llon, rlon)})
+        x = u_comp.coords['lon'].values - 180 + 85
+        y = u_comp.coords['lat'].values + 18.5
+        U = u_comp.values; V = v_comp.values
+        
+        skip = 2
+        Q = ax.quiver(x[::skip], y[::skip], U[::skip, ::skip], V[::skip, ::skip],
+                    pivot='mid', units='inches', scale=25, scale_units='width',
+                    headwidth=6, headlength=4, headaxislength=3)
+        qk = plt.quiverkey(Q, 0.5, 0.05, 2, '2 m/s', coordinates='figure')
+
+        # bounding box and center
+        ax.plot([75, 95, 95, 75, 75], [10, 10, 27, 27, 10], 'r-', lw=1.0, transform=proj)
+        ax.plot(85, 18.5, 'ro', ms=3, transform=proj)
+        ax.set_title('{}\nlag ${}$'.format(title, lag))
+        plt.tight_layout(pad=5)
+        #fname = '/global/home/users/hpeter/images/winds' + str(lvl) + '_biweekly1234s.png'
+        fname = '/home/hpeter/Research2018/images/call20180712/winds{}mb_{}{}{}_lag{}.png'.format(lvl, mode, phases, amp[0], lag)
+        plt.savefig(fname, dpi=120)
+def plot2dxy_lag_vector(ufilenames, vfilenames,
+                        ufilenames_ttest=None, 
+                        vfilenames_ttest=None, 
+                        lags=[-2,-1,0,1,2], 
+                        lvl=850, mode='biweekly', 
+                        phases='1234', 
+                        amp='strong'):
+    # setup
+    title='Significant Anomalous Wind\n({} mb, n = {}, {} {} mode phases {})'.format(lvl, n, amp, mode, phases) 
     proj = ccrs.PlateCarree()
     f = plt.figure(figsize=(16,5))
     #f = plt.figure(figsize=(10,10))
@@ -94,7 +181,6 @@ def plot2dxy_lag_vector(ufilenames, vfilenames, ufilenames_ttest=None, vfilename
     elif len(lags) == 1:
         nR = 1; nC = 1
         nums = [1]
-    #llon = 150; rlon = 210; blat = -25; tlat = 25
     llon = 130; rlon = 230; blat = -55; tlat = 30
 
     # loop
@@ -152,28 +238,23 @@ def plot2dxy_lag_vector(ufilenames, vfilenames, ufilenames_ttest=None, vfilename
         y = u_comp.coords['lat'].values + 18.5
         U = u_comp.values; V = v_comp.values
         
-        #skip = 1
         skip = 3
         Q = ax.quiver(x[::skip], y[::skip], U[::skip, ::skip], V[::skip, ::skip],
                     pivot='mid', units='inches', scale=25, scale_units='width',
-                    headwidth=10, headlength=5, headaxislength=3)
-                    #pivot='mid', units='inches', scale=50, scale_units='width',
-                    #headwidth=5, headlength=2.5, headaxislength=1.5, lw=0.5)
+                    headwidth=10, headlength=5, headaxislength=4)
         qk = plt.quiverkey(Q, 0.5, 0.15, 2, '2 m/s', coordinates='figure')
-        #qk = plt.quiverkey(Q, 0.95, 0.95, 2, '2 m/s', coordinates='figure')
 
         # bounding box and center
         ax.plot([75, 95, 95, 75, 75], [10, 10, 27, 27, 10], 'r-', lw=1.0, transform=proj)
         ax.plot(85, 18.5, 'ro', ms=3, transform=proj)
         if i == 2:
-        #if i == 0:
             ax.set_title('{}\nlag ${}$'.format(title, lag))
         else:
             ax.set_title('lag ${}$'.format(lag))
     plt.tight_layout(pad=3, w_pad=-4)
-    #plt.tight_layout(pad=3, w_pad=-3)
-    fname = '/global/home/users/hpeter/images/winds' + str(lvl) + '_biweekly1234s.png'
-    plt.savefig(fname, dpi=100)
+    #fname = '/global/home/users/hpeter/images/winds' + str(lvl) + '_biweekly1234s.png'
+    fname = '/home/hpeter/Research2018/images/call20180712/winds{}mb_{}{}{}.png'.format(lvl, mode, phases, amp[0])
+    plt.savefig(fname, dpi=120)
 
 def plot2dxz_lag(filenames, title='Default lags, ttest', lags=[-2,-1,0,1,2], ttest=True):
     # setup
@@ -228,60 +309,39 @@ def plot2dxz_lag(filenames, title='Default lags, ttest', lags=[-2,-1,0,1,2], tte
             ax.set_title('lag {}'.format(lag))
     plt.tight_layout(pad=1, w_pad=-5)
     fname = 'verticalV_ttest.png'
-    #plt.savefig(fname, dpi=100)
+    #plt.savefig(fname, dpi=120)
 
 lags0 = [0]
 lags02 = [-2, 0, 2]
 lags012 = [-2, -1, 0, 1, 2]
 
-idir = '/global/scratch/hpeter/composites/'
+#idir = '/global/scratch/hpeter/composites/'
+idir = '/home/hpeter/Research2018/MD_files/composites/'
 
 #plot1dx('composite_n150_Vanom_JJAS_lag0.nc')
 
 #plot2dxy_lag('composite_n150_V_JJAS_strat_harm_ttest_lag', lags=lags012, lvl=850)
 #plot2dxy_lag('composite_n150_V_JJAS_strat_harm_ttest_lag', lags=lags012, lvl=300)
 
-plot2dxy_lag_vector('composite_n150_U_JJAS_biweekly1234s_harm_lag', 
-                    'composite_n150_V_JJAS_biweekly1234s_harm_lag', 
-                    'composite_n150_U_JJAS_biweekly1234s_harm_ttest_lag',
-                    'composite_n150_V_JJAS_biweekly1234s_harm_ttest_lag',
-                    title='Significant Anomalous Wind\n(850 mb, n = 150, strong biweekly mode phases 1-4)', 
-                    lags=lags012, 
-                    lvl=850)
-plot2dxy_lag_vector('composite_n150_U_JJAS_biweekly1234s_harm_lag', 
-                    'composite_n150_V_JJAS_biweekly1234s_harm_lag', 
-                    'composite_n150_U_JJAS_biweekly1234s_harm_ttest_lag',
-                    'composite_n150_V_JJAS_biweekly1234s_harm_ttest_lag',
-                    title='Significant Anomalous Wind\n(500 mb, n = 150, strong biweekly mode phases 1-4)', 
-                    lags=lags012, 
-                    lvl=500)
-plot2dxy_lag_vector('composite_n150_U_JJAS_biweekly1234s_harm_lag', 
-                    'composite_n150_V_JJAS_biweekly1234s_harm_lag', 
-                    'composite_n150_U_JJAS_biweekly1234s_harm_ttest_lag',
-                    'composite_n150_V_JJAS_biweekly1234s_harm_ttest_lag',
-                    title='Significant Anomalous Wind\n(300 mb, n = 150, strong biweekly mode phases 1-4)', 
-                    lags=lags012, 
-                    lvl=300)
-#plot2dxy_lag_vector('composite_n150_U_JJAS_weekly2345s_harm_lag', 
-#                    'composite_n150_V_JJAS_weekly2345s_harm_lag', 
-#                    'composite_n150_U_JJAS_weekly2345s_harm_ttest_lag',
-#                    'composite_n150_V_JJAS_weekly2345s_harm_ttest_lag',
-#                    title='Significant Anomalous Wind\n(850 mb, n = 150, strong weekly mode phases 2-5)', 
-#                    lags=lags012, 
-#                    lvl=850)
-#plot2dxy_lag_vector('composite_n150_U_JJAS_weekly2345s_harm_lag', 
-#                    'composite_n150_V_JJAS_weekly2345s_harm_lag', 
-#                    'composite_n150_U_JJAS_weekly2345s_harm_ttest_lag',
-#                    'composite_n150_V_JJAS_weekly2345s_harm_ttest_lag',
-#                    title='Significant Anomalous Wind\n(500 mb, n = 150, strong weekly mode phases 2-5)', 
-#                    lags=lags012, 
-#                    lvl=500)
-#plot2dxy_lag_vector('composite_n150_U_JJAS_weekly2345s_harm_lag', 
-#                    'composite_n150_V_JJAS_weekly2345s_harm_lag', 
-#                    'composite_n150_U_JJAS_weekly2345s_harm_ttest_lag',
-#                    'composite_n150_V_JJAS_weekly2345s_harm_ttest_lag',
-#                    title='Significant Anomalous Wind\n(300 mb, n = 150, strong weekly mode phases 2-5)', 
-#                    lags=lags012, 
-#                    lvl=300)
-
+phases = '1234'
+#phases = '5678'
+#phases = '2345'
+#phases = '1678'
+mode   = 'biweekly'
+#mode   = 'weekly'
+amp    = 'strong'
+n      = '150'
+#n      = '113'
+lvls   = [850, 500, 300]
+for lvl in lvls:
+    plot2dxy_lag_vector_indiv('composite_n{}_U_JJAS_{}{}{}_harm_lag'.format(n, mode, phases, amp[0]), 
+                        'composite_n{}_V_JJAS_{}{}{}_harm_lag'.format(n, mode, phases, amp[0]), 
+                  'composite_n{}_U_JJAS_{}{}{}_harm_ttest_lag'.format(n, mode, phases, amp[0]),
+                  'composite_n{}_V_JJAS_{}{}{}_harm_ttest_lag'.format(n, mode, phases, amp[0]),
+                   lags=lags012, lvl=lvl, mode=mode, phases=phases, amp=amp)
+    plot2dxy_lag_vector('composite_n{}_U_JJAS_{}{}{}_harm_lag'.format(n, mode, phases, amp[0]), 
+                        'composite_n{}_V_JJAS_{}{}{}_harm_lag'.format(n, mode, phases, amp[0]), 
+                  'composite_n{}_U_JJAS_{}{}{}_harm_ttest_lag'.format(n, mode, phases, amp[0]),
+                  'composite_n{}_V_JJAS_{}{}{}_harm_ttest_lag'.format(n, mode, phases, amp[0]),
+                   lags=lags012, lvl=lvl, mode=mode, phases=phases, amp=amp)
 plt.show()
